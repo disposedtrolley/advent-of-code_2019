ecba265f1f622e11882368c458d9032b08461fd3
James Liu <james_liu@fastmail.com>
2019-12-03 22:05:05 +1100

    feat: reorg folders, add day 1 part 1

--- main.go
+++ main.go
@@ -1,28 +1,8 @@
 package main
 
-import (
-	"io/ioutil"
-	"log"
-	"os"
-	"strings"
-)
+import "github.com/disposedtrolley/advent-of-code_2019/day1"
 
-// ReadFromFile reads the file at filepath and returns the contents as a string.
-func ReadFromFile(filepath string) string {
-	if _, err := os.Stat(filepath); os.IsNotExist(err) {
-		log.Fatalf("open %s: %v", filepath, err)
-	}
+func main() {
 
-	dat, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(dat)
-}
-
-// StringToLines separates a string s into an array of strings, separating at
-// newlines.
-func StringToLines(s string) []string {
-	return strings.Split(s, "\n")
+	day1.Run("./day1/day1.txt")
 }
