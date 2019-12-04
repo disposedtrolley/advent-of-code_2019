ae8b08634694ec1cf94d14a9fbdc8253e2219f09
James Liu <james_liu@fastmail.com>
2019-12-04 21:46:01 +1100

    feat: add day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -29,6 +29,18 @@ import (
 
 // What is the sum of the fuel requirements for all of the modules on your spacecraft?
 
+// --- Part Two ---
+// During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+
+// Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+
+// So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+// A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+// At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+// The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+// What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
+
 type Spacecraft struct {
 	Modules []*Module
 }
@@ -39,7 +51,8 @@ func (s *Spacecraft) AddModule(m *Module) {
 
 func (s *Spacecraft) TotalFuelRequired() (fuel int) {
 	for _, m := range s.Modules {
-		fuel += m.FuelRequired()
+		fuel += m.TotalFuel()
+		fmt.Printf("totalFuel: %d\n", fuel)
 	}
 
 	return fuel
@@ -49,8 +62,20 @@ type Module struct {
 	Mass int
 }
 
-func (m *Module) FuelRequired() (fuel int) {
-	return int(math.Floor(float64(m.Mass)/3) - 2)
+func (m *Module) TotalFuel() (fuel int) {
+	fuel = m.fuelForMass(m.Mass)
+
+	fuelForFuel := m.fuelForMass(fuel)
+	for fuelForFuel >= 0 {
+		fuel += fuelForFuel
+		fuelForFuel = m.fuelForMass(fuelForFuel)
+	}
+
+	return fuel
+}
+
+func (m *Module) fuelForMass(mass int) (fuel int) {
+	return int(math.Floor(float64(mass)/3) - 2)
 }
 
 func Run(inputFile string) {
