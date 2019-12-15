67b295eb4caa69b2a38722269b95c34aeb21a7e6
James Liu <james_liu@fastmail.com>
2019-12-15 12:57:57 +1100

    feat: add mass function and tests

---
+++ src/aoc/day1.clj
@@ -0,0 +1,20 @@
+(ns aoc.day1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+;; Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+
+;; For example:
+
+;; For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+;; For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+;; For a mass of 1969, the fuel required is 654.
+;; For a mass of 100756, the fuel required is 33583.
+
+(def modules (str/split
+              (slurp (io/resource "day1.txt"))
+              #"\n"))
+
+(defn mass
+  ([module]
+   (- (int (/ module 3)) 2)))
---
+++ test/aoc/day1_test.clj
@@ -0,0 +1,13 @@
+(ns aoc.day1-test
+  (:require [clojure.test :refer :all]
+            [aoc.day1 :refer :all]))
+
+(deftest test-mass
+  (def tests [{:input 12 :expected 2}
+              {:input 14 :expected 2}
+              {:input 1969 :expected 654}
+              {:input 100756 :expected 33583}])
+
+  (doseq [t tests]
+    (is (= (mass (get t :input)) (get t :expected))))
+)
